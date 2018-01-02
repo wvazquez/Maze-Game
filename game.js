a66820fd42fbc55dccc08d4e0457adef0e99f20c
Wilmer Vazquez <vazquezwilmer@gmail.com>
2018-01-02 13:08:10 -0500

    Adds left, right, jump movement to our character.

--- game.js
+++ game.js
@@ -2,6 +2,7 @@
 var game;
 var platforms;
 var player;
+var cursors;
 
 //Sets up our game
 game = new Phaser.Game(800, 600, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
@@ -39,10 +40,44 @@ function create() {
   player.body.gravity.y = 300;
   player.body.collideWorldBounds = true;
 
+  //
+  // player.animations.add('left', [0, 1, 2, 3], 10, true);
+  // player.animations.add('right', [5, 6, 7, 8], 10, true);
+  player.animations.add('walk');
+  cursors = game.input.keyboard.createCursorKeys();
 }
 //checks when player and platform collide.
 function update() {
   var hitPlatform = game.physics.arcade.collide(player, platforms);
+
+  //  Reset the players velocity (movement)
+   player.body.velocity.x = 0;
+
+   if (cursors.left.isDown)
+   {
+       //  Move to the left
+       player.animations.play('walk', 10, true);
+       player.body.velocity.x = -150;
+       player.scale.x = -1;
+   }
+   else if (cursors.right.isDown)
+   {
+       //  Move to the right
+       player.animations.play('walk', 10, true);
+       player.body.velocity.x = 150;
+       player.scale.x = 1;
+   }
+   else
+   {
+       //  Stand still is no button is pressed
+       player.animations.stop();
+   }
+
+   //  Allows the player to jump if they are touching the ground.
+   if (cursors.up.isDown && player.body.touching.down && hitPlatform){
+       player.body.velocity.y = -300;
+   }
+
 }
 
 function render(){
